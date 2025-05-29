0e30fe03d090f0a2e636b24a83a679eb125125c0
cccaaannn <can.kurt.aa@gmail.com>
2025-05-29 23:12:23 +0300

    router

--- src/trpc/root-router.ts
+++ src/trpc/root-router.ts
@@ -1,8 +1,16 @@
 import { createTRPCRouter } from "@/trpc/init";
+import { furnitureRouter } from "@/trpc/routes/furniture-router";
+import { itemRouter } from "@/trpc/routes/item-router";
 import { roomRouter } from "@/trpc/routes/room-router";
+import { sectionRouter } from "@/trpc/routes/section-router";
+import { tagRouter } from "@/trpc/routes/tag-router";
 
 
 export const trpcRouter = createTRPCRouter({
-	room: roomRouter
+	room: roomRouter,
+	furniture: furnitureRouter,
+	section: sectionRouter,
+	item: itemRouter,
+	tag: tagRouter
 });
 export type TRPCRouter = typeof trpcRouter;
