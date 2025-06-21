cbf225b772555090688987c26ce7465beab26d09
cccaaannn <can.kurt.aa@gmail.com>
2025-06-22 00:34:46 +0300

    error page updated

--- src/components/generic-error.tsx
+++ src/components/generic-error.tsx
@@ -1,6 +1,12 @@
 const GenericError = () => {
 	return (
-		<div>Something went wrong</div>
+		<div className="text-center h-full">
+			<div className="h-full flex flex-col items-center justify-center gap-2">
+				<h1 className="text-2xl font-bold">Error</h1>
+
+				<p className="text-gray-500">An unexpected error has occurred.</p>
+			</div>
+		</div>
 	);
 };
 
