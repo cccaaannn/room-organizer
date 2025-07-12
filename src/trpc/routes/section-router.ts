11901e2ce0d0d794488e56acd9832d673cd67924
cccaaannn <can.kurt.aa@gmail.com>
2025-07-13 02:31:10 +0300

    section trpc router updated

--- src/trpc/routes/section-router.ts
+++ src/trpc/routes/section-router.ts
@@ -1,23 +1,27 @@
 import type { TRPCRouterRecord } from "@trpc/server";
-import { desc, eq, sql } from "drizzle-orm";
+import { and, desc, eq, sql } from "drizzle-orm";
 import { z } from "zod";
 
 import db from "@/db/init";
 import { sections } from "@/db/schema";
-import { publicProcedure } from "@/trpc/init";
+import { protectedProcedure } from "@/trpc/init";
 
 
 export const sectionRouter = {
-	getAll: publicProcedure.query(() =>
+	getAll: protectedProcedure.query(opts =>
 		db.query.sections.findMany({
+			where: eq(sections.userId, opts.ctx.session!.user.id),
 			orderBy: o => [desc(sql`coalesce(${o.updatedAt}, ${o.createdAt})`)]
 		})
 	),
-	getByFurnitureId: publicProcedure
+	getByFurnitureId: protectedProcedure
 		.input(z.string())
 		.query(opts =>
 			db.query.sections.findMany({
-				where: eq(sections.furnitureId, opts.input),
+				where: and(
+					eq(sections.userId, opts.ctx.session!.user.id),
+					eq(sections.furnitureId, opts.input)
+				),
 				with: {
 					furniture: true
 				},
