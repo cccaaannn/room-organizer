7a9566019caecaa7f183e3435a4b767cb2279bfb
cccaaannn <can.kurt.aa@gmail.com>
2025-08-24 00:04:57 +0300

    tag remove

--- src/routes/tags/-server-functions/remove.ts
+++ src/routes/tags/-server-functions/remove.ts
@@ -1,9 +1,10 @@
 import { createServerFn } from "@tanstack/react-start";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
 import db from "@/db/init";
 import { itemsTags, tags } from "@/db/schema";
+import { currentUserMiddleware } from "@/middlewares/current-user-middleware";
 
 
 const RemoveTagScheme = z.object({
@@ -12,10 +13,14 @@ const RemoveTagScheme = z.object({
 
 const removeTag = createServerFn({ method: "POST", response: "full" })
 	.validator(f => RemoveTagScheme.parse(f))
+	.middleware([currentUserMiddleware])
 	.handler(async ctx => {
 		const entity = await db.select()
 			.from(tags)
-			.where(eq(tags.id, ctx.data.id));
+			.where(and(
+				eq(tags.id, ctx.data.id),
+				eq(tags.userId, ctx.context.user.id)
+			));
 
 		if (entity.length === 0) {
 			throw new Error("Not found");
