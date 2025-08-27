a8bc1736b85cdc2ba179f34ecfab9207c161ce5e
cccaaannn <can.kurt.aa@gmail.com>
2025-08-27 05:21:21 +0300

    header updated

--- src/components/layout/app-layout/header.tsx
+++ src/components/layout/app-layout/header.tsx
@@ -1,33 +1,113 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { toast } from "react-toastify";
 
 import ModeToggle from "@/components/shadcn/hooks/theme/mode-toggle";
+import { Button } from "@/components/shadcn/ui/button";
 import { Separator } from "@/components/shadcn/ui/separator";
+import { signOut, useSession } from "@/lib/auth-client";
 
 
-const header = () => {
+const Header = () => {
+
+	const navigate = useNavigate();
+
+	const { data: session } = useSession();
+	const loggedIn = !!session?.user;
+
+	const onSignout = async () => {
+		await signOut();
+		toast("Signed out");
+		navigate({ to: "/" });
+	};
+
 	return (
 		<header className="p-2 sticky top-0 bg-gray-100 dark:bg-gray-900 border-b border-gray-300 dark:border-gray-700 z-10">
 			<nav className="flex gap-2 font-medium justify-between items-center">
 				<div className="flex gap-2 font-medium h-5">
-					<Link to="/">Home</Link>
+					<Link
+						to="/"
+						className="hover:underline"
+					>
+						<img src="/image/icon-500x500.png" alt="Logo" className="h-6" />
+					</Link>
 
 					<Separator orientation="vertical" className="bg-gray-400" />
 
-					<Link to="/rooms">Rooms</Link>
+					{
+						loggedIn ?
+							<>
+								<Link
+									to="/rooms"
+									className="hover:underline"
+								>
+									Rooms
+								</Link>
 
-					<Separator orientation="vertical" className="bg-gray-400" />
+								<Separator orientation="vertical" className="bg-gray-400" />
 
-					<Link to="/tags">Tags</Link>
+								<Link
+									to="/tags"
+									className="hover:underline"
+								>
+									Tags
+								</Link>
 
-					<Separator orientation="vertical" className="bg-gray-400" />
+								<Separator orientation="vertical" className="bg-gray-400" />
 
-					<Link to="/items">All items</Link>
+								<Link
+									to="/items"
+									className="hover:underline"
+								>
+									All items
+								</Link>
+
+								<Separator orientation="vertical" className="bg-gray-400" />
+
+								<Button
+									variant="link"
+									size="sm"
+									className="p-0 h-6 text-md hover:cursor-pointer text-red-400 hover:text-red-500"
+									onClick={onSignout}
+								>
+									Sign out
+								</Button>
+							</>
+							:
+							<>
+								<Link
+									to="/auth/signup"
+									className="hover:underline"
+								>
+									Sign up
+								</Link>
+
+								<Separator orientation="vertical" className="bg-gray-400" />
+
+								<Link
+									to="/auth/signin"
+									className="hover:underline"
+								>
+									Sign in
+								</Link>
+							</>
+					}
 				</div>
 
-				<ModeToggle />
+				<div
+					className="flex items-center gap-2 text-gray-500 dark:text-gray-400"
+				>
+					{
+						loggedIn &&
+						<p>
+							{session.user?.email}
+						</p>
+					}
+
+					<ModeToggle />
+				</div>
 			</nav>
-		</header>
+		</header >
 	);
 };
 
-export default header;
+export default Header;
